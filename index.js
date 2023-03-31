25081d8a07d7ff5bf9897f7127b9bb251fbd4e21
Brenden Olding <oldingb21@gmail.com>
2023-03-31 15:45:12 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let message;
+  if (feet <= 400) {
+    message = "This one is on me!";
+  } else if (feet >= 400 && feet <= 2000 ) {
+    message = "That will be twenty bucks.";
+  } else if (feet > 2000 && feet <= 2500) {
+    message = "I will gladly take your thirty bucks."
+  } else {
+    message = "No can do."
+  }
+  return message
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let message = (city === 'NYC') ? "Ok, sounds good." : "No go.";
+  return message
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let message;
+  switch(tip) {
+    case 'generous' :
+      message = 'Thank you so much.';
+      break;
+    case 'not as generous' :
+      message = 'Thank you.';
+      break;
+    default :
+      message = 'Bye.'
+  }
+  return message
 }
